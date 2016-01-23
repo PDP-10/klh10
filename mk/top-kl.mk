74e63d2b600ef7c555eccc7de969288ce0ad9638
Olaf Seibert <rhialto@falu.nl>
2016-01-23 23:19:19 +0100

    Eliminate recursive Make invocation
    by moving the model-specific CONFFLAGS into the model-specific
    makefile fragments.

--- mk/top-kl.mk
+++ mk/top-kl.mk
@@ -1,5 +1,25 @@
 # Makefile fragment for building kl
 
-all:	base-kl tapedd vdkfmt wxtest enaddr
+####################################################################
+##	Specific KLH10 configuration
+##
+
+# Standard setup for KL (TOPS-10 and TOPS-20)
+#
+CONFFLAGS = -DKLH10_CPU_KLX=1	\
+	    -DKLH10_SYS_T20=1	\
+	    -DKLH10_EVHS_INT=1	\
+		    -DKLH10_DEV_DPNI20=1 \
+		    -DKLH10_DEV_DPTM03=1 \
+		    -DKLH10_DEV_DPRPXX=1 \
+	    -DKLH10_MEM_SHARED=1	\
+	    -DKLH10_RTIME_OSGET=1	\
+	    -DKLH10_ITIME_INTRP=1	\
+	    -DKLH10_CTYIO_INT=1	\
+	    -DKLH10_APRID_SERIALNO=3600 \
+	    -DKLH10_CLIENT=\"MyKL\" \
+	    $(CONFFLAGS_AUX)
+
+all:	kn10-kl tapedd vdkfmt wxtest enaddr
 
 #---
--- mk/top-ks-its.mk
+++ mk/top-ks-its.mk
@@ -1,5 +1,26 @@
 # Makefile fragment for building ks-its
 
-all:	base-ks-its tapedd vdkfmt wxtest enaddr
+####################################################################
+##	Specific KLH10 configuration
+##
+
+# Standard setup for KS ITS
+#
+CONFFLAGS = -DKLH10_CPU_KS=1	\
+	    -DKLH10_SYS_ITS=1	\
+	    -DKLH10_EVHS_INT=1	\
+		    -DKLH10_DEV_DPTM03=1 \
+		    -DKLH10_DEV_DPRPXX=1 \
+		    -DKLH10_DEV_DPIMP=1 \
+	    -DKLH10_SIMP=0 \
+	    -DKLH10_MEM_SHARED=1 \
+	    $(TINTFLAGS) \
+	    $(DINTFLAGS) \
+	    -DKLH10_APRID_SERIALNO=4097 -DKLH10_DEVMAX=12 \
+	    -DKLH10_CLIENT=\"MyITS\" \
+	    $(CONFFLAGS_AUX) \
+	    -DVMTAPE_ITSDUMP=1
+
+all:	kn10-ks-its tapedd vdkfmt wxtest enaddr
 
 #---
