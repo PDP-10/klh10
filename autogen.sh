dea808081877968bcfad41ff076d50faa551e917
Olaf Seibert <rhialto@falu.nl>
2016-02-24 17:36:15 +0100

    "Discourage" building directly in the source tree.

--- autogen.sh
+++ autogen.sh
@@ -5,19 +5,28 @@
 # Usage: ../path/to/autogen.sh [args for configure]
 #
 # (Re)constructs the configure script using the appropriate invocations
-# of autoconf etc, and runs configure with the given arguments.
+# of autoconf etc.
 #
 # It is strongly recommended to build in a separate directory
 # for easy cleanup.
+#
+# When this is done, it runs configure with the given arguments.
 
 srcdir=$(dirname $0)
 test -z "$srcdir" && srcdir=.
 
 builddir=$(pwd)
 cd "$srcdir"
+srcdir=$(pwd)
 
-autoconf -I aclocal
-autoheader
+autoconf	# creates configure from configure.ac
+autoheader	# creates src/config.h.in from configure.ac
 
-cd "$builddir"
-$srcdir/configure "$@"
+if [ "$srcdir" != "$builddir" ]
+then
+    echo "Now running configure $@ in $builddir"
+    cd "$builddir"
+    $srcdir/configure "$@"
+else
+    echo "Not running configure in the source directory."
+fi
