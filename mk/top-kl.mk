2c674266ea4085bac8e9a978cf5bec73710e4c66
Olaf Seibert <rhialto@falu.nl>
2021-07-19 19:59:04 +0200

    Move stuff in the Makefiles around
    so that the cpu model specific parts are moved (more than before) into
    their own files.

--- mk/top-kl.mk
+++ mk/top-kl.mk
@@ -21,4 +21,83 @@ CONFFLAGS = -DKLH10_CPU_KLX=1	\
 
 all:	kn10-kl DPROCS_KL ALL_UTILS
 
+####################################################################
+##
+##	Basic KN10 configurations
+##
+
+# Modules needed for KL10 version.
+
+OFILES_KL = klh10.o prmstr.o fecmd.o feload.o wfio.o osdsup.o \
+	kn10cpu.o kn10pag.o kn10clk.o opdata.o kn10ops.o \
+	inmove.o inhalf.o inblsh.o intest.o \
+	infix.o  inflt.o  inbyte.o injrst.o \
+	inexts.o inio.o   kn10dev.o 	\
+	dvcty.o  dvdte.o	\
+	vdisk.o  dvrpxx.o dvrh20.o	\
+	vmtape.o dvtm03.o	\
+	dvni20.o dpsup.o	\
+	dvhost.o dvlites.o
+
+kn10-kl: $(OFILES_KL)
+	$(LINKER) $(LDFLAGS) $(LDOUTF) kn10-kl $(OFILES_KL) $(LIBS) $(CPULIBS)
+
+####################################################################
+##	Specific KLH10 configurations
+##	
+##	Provided as a convenience, not intended to satisfy all
+##	possible platforms or configurations.
+
+# Standard setup for KL (TOPS-10 and TOPS-20)
+#
+base-kl:
+	$(MAKER) kn10-kl $(DPROCS_KL) $(BASE_UTILS) uexbconv \
+	    "SRC = $(SRC)" \
+	    "CC = $(CC)" \
+	    "CFLAGS = $(CFLAGS) $(CFLAGS_AUX)" \
+	    "CPPFLAGS = $(CPPFLAGS)" \
+	    "LDFLAGS = $(LDFLAGS)" \
+	    "LIBS = $(LIBS)" \
+	    "CPULIBS = $(CPULIBS)" \
+	    "NETLIBS = $(NETLIBS)" \
+	    "CENVFLAGS = $(CENVFLAGS)" \
+	    "CONFFLAGS = \
+		-DKLH10_CPU_KLX=1	\
+		-DKLH10_SYS_T20=1	\
+		-DKLH10_EVHS_INT=1	\
+			-DKLH10_DEV_DPNI20=1 \
+			-DKLH10_DEV_DPTM03=1 \
+			-DKLH10_DEV_DPRPXX=1 \
+		-DKLH10_MEM_SHARED=1	\
+		-DKLH10_RTIME_OSGET=1	\
+		-DKLH10_ITIME_INTRP=1	\
+		-DKLH10_CTYIO_INT=1	\
+		-DKLH10_APRID_SERIALNO=3600 \
+		-DKLH10_CLIENT=\\\"MyKL\\\" \
+		$(CONFFLAGS_AUX) \
+		$(CONFFLAGS_USR) "
+
+base-ks:
+	@echo "base-ks is not built in this directory."
+
+base-ks-its:
+	@echo "base-ks-its is not built in this directory."
+
+####################################################################
+##	Lintish versions to see how many compiler warnings we can generate
+##
+
+lint-kl:
+	$(MAKER) kn10-kl $(DPROCS_KL) $(BASE_UTILS) uexbconv \
+	    "SRC = $(SRC)" \
+	    "CC = $(CC)" \
+	    "CFLAGS = $(CFLAGS) $(CFLAGS_AUX) $(CFLAGS_LINT)" \
+	    "CPPFLAGS = $(CPPFLAGS)" \
+	    "LDFLAGS = $(LDFLAGS)" \
+	    "LIBS = $(LIBS)" \
+	    "CPULIBS = $(CPULIBS)" \
+	    "NETLIBS = $(NETLIBS)" \
+	    "CENVFLAGS = $(CENVFLAGS)" \
+	    "CONFFLAGS = $(CONFFLAGS) $(CONFFLAGS_AUX) $(CONFFLAGS_USR)"
+
 #---
--- mk/top-ks.mk
+++ mk/top-ks.mk
@@ -19,4 +19,82 @@ CONFFLAGS = -DKLH10_CPU_KS=1	\
 
 all:	kn10-ks DPROCS_KS ALL_UTILS
 
+####################################################################
+##
+##	Basic KN10 configurations
+##
+
+# Modules needed for KS10 version.
+
+OFILES_KS = klh10.o prmstr.o fecmd.o feload.o wfio.o osdsup.o \
+	kn10cpu.o kn10pag.o kn10clk.o opdata.o kn10ops.o \
+	inmove.o inhalf.o inblsh.o intest.o \
+	infix.o  inflt.o  inbyte.o injrst.o \
+	inexts.o inio.o   kn10dev.o dvuba.o  \
+	dvcty.o  			\
+	vdisk.o  dvrpxx.o dvrh11.o	\
+	vmtape.o dvtm03.o	\
+	dvlhdh.o dvdz11.o dvch11.o \
+	dpsup.o \
+	dvhost.o dvlites.o
+
+kn10-ks: $(OFILES_KS)
+	$(LINKER) $(LDFLAGS) $(LDOUTF) kn10-ks $(OFILES_KS) $(LIBS) $(CPULIBS)
+
+####################################################################
+##	Specific KLH10 configurations
+##	
+##	Provided as a convenience, not intended to satisfy all
+##	possible platforms or configurations.
+
+# Standard setup for KS (TOPS-20, maybe TOPS-10)
+#
+base-ks:
+	$(MAKER) kn10-ks $(DPROCS_KS) $(BASE_UTILS) \
+	    "SRC = $(SRC)" \
+	    "CC = $(CC)" \
+	    "CFLAGS = $(CFLAGS) $(CFLAGS_AUX)" \
+	    "CPPFLAGS = $(CPPFLAGS)" \
+	    "LDFLAGS = $(LDFLAGS)" \
+	    "LIBS = $(LIBS)" \
+	    "CPULIBS = $(CPULIBS)" \
+	    "NETLIBS = $(NETLIBS)" \
+	    "CENVFLAGS = $(CENVFLAGS)" \
+	    "CONFFLAGS = \
+		-DKLH10_CPU_KS=1	\
+		-DKLH10_SYS_T20=1	\
+		-DKLH10_EVHS_INT=1	\
+			-DKLH10_DEV_DPTM03=1 \
+			-DKLH10_DEV_DPRPXX=1 \
+		-DKLH10_MEM_SHARED=1 \
+		$(TINTFLAGS) \
+		$(DINTFLAGS) \
+		-DKLH10_APRID_SERIALNO=4097 \
+		-DKLH10_CLIENT=\\\"MyKS\\\" \
+		$(CONFFLAGS_AUX) \
+		$(CONFFLAGS_USR) "
+
+base-ks-its:
+	@echo "base-ks-its is not built in this directory."
+
+base-kl:
+	@echo "base-kl is not built in this directory."
+
+####################################################################
+##	Lintish versions to see how many compiler warnings we can generate
+##
+
+lint-ks:
+	$(MAKER) kn10-ks $(DPROCS_KS) $(BASE_UTILS) \
+	    "SRC = $(SRC)" \
+	    "CC = $(CC)" \
+	    "CFLAGS = $(CFLAGS) $(CFLAGS_AUX) $(CFLAGS_LINT)" \
+	    "CPPFLAGS = $(CPPFLAGS)" \
+	    "LDFLAGS = $(LDFLAGS)" \
+	    "LIBS = $(LIBS)" \
+	    "CPULIBS = $(CPULIBS)" \
+	    "NETLIBS = $(NETLIBS)" \
+	    "CENVFLAGS = $(CENVFLAGS)" \
+	    "CONFFLAGS = $(CONFFLAGS) $(CONFFLAGS_AUX) $(CONFFLAGS_USR)"
+
 #---
