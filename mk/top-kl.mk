f8510c75602ce41295f6b2d0b36647a70acdcc77
Olaf Seibert <rhialto@falu.nl>
2021-07-20 18:28:58 +0200

    Remove some old build stuff.

--- mk/top-kl.mk
+++ mk/top-kl.mk
@@ -19,7 +19,7 @@ CONFFLAGS = -DKLH10_CPU_KLX=1	\
 	    -DKLH10_APRID_SERIALNO=3600 \
 	    -DKLH10_CLIENT=\"MyKL\"
 
-all:	kn10-kl DPROCS_KL ALL_UTILS
+all base-kl:	kn10-kl DPROCS_KL ALL_UTILS
 
 ####################################################################
 ##
@@ -41,41 +41,6 @@ OFILES_KL = klh10.o prmstr.o fecmd.o feload.o wfio.o osdsup.o \
 
 kn10-kl: $(OFILES_KL)
 	$(LINKER) $(LDFLAGS) $(LDOUTF) kn10-kl $(OFILES_KL) $(LIBS) $(CPULIBS)
-
-####################################################################
-##	Specific KLH10 configurations
-##	
-##	Provided as a convenience, not intended to satisfy all
-##	possible platforms or configurations.
-
-# Standard setup for KL (TOPS-10 and TOPS-20)
-#
-base-kl:
-	$(MAKER) kn10-kl $(DPROCS_KL) $(BASE_UTILS) uexbconv \
-	    "SRC = $(SRC)" \
-	    "CC = $(CC)" \
-	    "CFLAGS = $(CFLAGS) $(CFLAGS_AUX)" \
-	    "CPPFLAGS = $(CPPFLAGS)" \
-	    "LDFLAGS = $(LDFLAGS)" \
-	    "LIBS = $(LIBS)" \
-	    "CPULIBS = $(CPULIBS)" \
-	    "NETLIBS = $(NETLIBS)" \
-	    "CENVFLAGS = $(CENVFLAGS)" \
-	    "CONFFLAGS = \
-		-DKLH10_CPU_KLX=1	\
-		-DKLH10_SYS_T20=1	\
-		-DKLH10_EVHS_INT=1	\
-			-DKLH10_DEV_DPNI20=1 \
-			-DKLH10_DEV_DPTM03=1 \
-			-DKLH10_DEV_DPRPXX=1 \
-		-DKLH10_MEM_SHARED=1	\
-		-DKLH10_RTIME_OSGET=1	\
-		-DKLH10_ITIME_INTRP=1	\
-		-DKLH10_CTYIO_INT=1	\
-		-DKLH10_APRID_SERIALNO=3600 \
-		-DKLH10_CLIENT=\\\"MyKL\\\" \
-		$(CONFFLAGS_AUX) \
-		$(CONFFLAGS_USR) "
 
 base-ks:
 	@echo "base-ks is not built in this directory."
--- mk/top-ks-its.mk
+++ mk/top-ks-its.mk
@@ -20,7 +20,7 @@ CONFFLAGS = -DKLH10_CPU_KS=1	\
 	    -DKLH10_CLIENT=\"MyITS\" \
 	    -DVMTAPE_ITSDUMP=1
 
-all:	kn10-ks-its DPROCS_KSITS ALL_UTILS
+all base-ks-its:	kn10-ks-its DPROCS_KSITS ALL_UTILS
 
 ####################################################################
 ##
@@ -43,43 +43,6 @@ OFILES_KS = klh10.o prmstr.o fecmd.o feload.o wfio.o osdsup.o \
 
 kn10-ks-its: $(OFILES_KS)
 	$(LINKER) $(LDFLAGS) $(LDOUTF) kn10-ks-its $(OFILES_KS) $(LIBS) $(CPULIBS)
-
-####################################################################
-##	Specific KLH10 configurations
-##	
-##	Provided as a convenience, not intended to satisfy all
-##	possible platforms or configurations.
-
-# Standard setup for KS ITS
-#
-base-ks-its:
-	$(MAKER) kn10-ks-its $(DPROCS_KSITS) $(BASE_UTILS) udlconv \
-	    "SRC = $(SRC)" \
-	    "CC = $(CC)" \
-	    "CFLAGS = $(CFLAGS) $(CFLAGS_AUX)" \
-	    "CPPFLAGS = $(CPPFLAGS)" \
-	    "LDFLAGS = $(LDFLAGS)" \
-	    "LIBS = $(LIBS)" \
-	    "CPULIBS = $(CPULIBS)" \
-	    "NETLIBS = $(NETLIBS)" \
-	    "CENVFLAGS = $(CENVFLAGS)" \
-	    "CONFFLAGS = \
-		-DKLH10_CPU_KS=1	\
-		-DKLH10_SYS_ITS=1	\
-		-DKLH10_EVHS_INT=1	\
-			-DKLH10_DEV_DPTM03=1 \
-			-DKLH10_DEV_DPRPXX=1 \
-			-DKLH10_DEV_DPIMP=1 \
-		-DKLH10_SIMP=0 \
-		-DKLH10_MEM_SHARED=1 \
-		$(TINTFLAGS) \
-		$(DINTFLAGS) \
-		-DKLH10_APRID_SERIALNO=4097 \
-		-DKLH10_CLIENT=\\\"MyITS\\\" \
-		$(CONFFLAGS_AUX) \
-		$(CONFFLAGS_USR) \
-		-DVMTAPE_ITSDUMP=1 "
-
 
 base-ks:
 	@echo "base-ks is not built in this directory."
